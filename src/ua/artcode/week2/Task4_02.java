f8024cb65f482e5c7e32754cb27145ffb69ea0b0
presly808 <presly808@gmail.com>
2016-06-06 07:50:07 +0300

    Update Task4_02.java

--- src/ua/artcode/week2/Task4_02.java
+++ src/ua/artcode/week2/Task4_02.java
@@ -11,29 +11,32 @@ public class Task4_02 {
         /*
         2) Поменять местами наибольший и наименьший элементы в массиве
          */
-        int [] mas = new int[]{2,1,23,23,1,200,51,-12,235,-4,-123, 121};
+        int[] mas = {2,1,23,23,1,200,51,-12,235,-4,-123, 121};
         System.out.println(Arrays.toString(mas));
+        
         int minValue, maxValue, minValuePos=0, maxValuePos=0;
-        minValue=Integer.MAX_VALUE;
-        maxValue=Integer.MIN_VALUE;
+        minValue = mas[0]; // init min and max by 
+        maxValue = mas[0];
+        // do some line breaks
+        
         for(int i=0; i<mas.length; i++) {
             if(mas[i]>maxValue) {
                 maxValue = mas[i];
                 maxValuePos = i;
             }
+            
             if(mas[i]<minValue) {
                 minValue = mas[i];
                 minValuePos = i;
             }
         }
 
+        // can be extract to a method
         int temp = mas[minValuePos];
         mas[minValuePos] = mas[maxValuePos];
         mas[maxValuePos] = temp;
 
         System.out.println(Arrays.toString(mas));
-
-
 
     }
 }
